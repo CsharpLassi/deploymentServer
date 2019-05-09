82bbc342052d4b2520358d7489a2fec87d30eb40
Lassi <csharplassi@posteo.de>
2019-05-09 14:57:57 +0200

    add app.db and  app.migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,25 @@
 import os
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
+from config import Config
 from .webhooks import webhook
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     """ Create, configure and return the Flask application """
     app = Flask(__name__)
-    app.config['GITHUB_SECRET'] = os.environ.get('GITHUB_SECRET')
-    app.config['REPO_PATH'] = os.environ.get('REPO_PATH')
+    app.config.from_object(Config)
+
+    db.init_app(db)
+    migrate.init_app(db, db)
+
+    from app import models
+
     app.register_blueprint(webhook)
 
     return app
